4cac6aad9e37e5197795c450cd0e840e198fe176
Caleb Glorvigen <jcglorvi@gmail.com>
2021-02-22 22:59:23 -0500

    Added a layout and imported the correct components

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
-import Project from '../../components/Project/Project'
-import db from './db.json';
+import React from "react";
+import Project from "../../components/Project/Project";
+import db from "./db.json";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import { Row, Col, Container, Navbar, Nav } from "react-bootstrap";
 
 const Portfolio = () => {
+  console.log(db);
 
-    console.log(db);
+  return (
+    <>
+      <Header>Yessir!!
 
-    return (
-        <div>
-            Portfolio page
-            {
-                db.map((item) => {
-                    return <Project key={item.id} {...item} />;
-                })
-            }
-        </div>
-    );
+      </Header>
+      <Row>
+        <Col>
+          <Container lg={8} className="bg-white">
+            <Row>
+              <Col lg>
+                Portfolio page
+                <Row>
+                  <Col>
+                    {db.map((item) => {
+                      return <Project key={item.id} {...item} />;
+                    })}
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default Portfolio;
