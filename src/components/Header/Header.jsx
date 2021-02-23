15ce2ac82dabd8ef2cc644bf275e3a3b24ef2021
Caleb Glorvigen <jcglorvi@gmail.com>
2021-02-22 23:06:38 -0500

    Got the Navbar set up

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Row, Col, Container, Navbar, Nav, NavDropdown, Form, Button, FormControl} from "react-bootstrap";
+
 
 const Header = () => {
     return (
-        <div>
-            Hello! this is the Header
-        </div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#/">
+          <b>Caleb Glorvigen</b>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#/">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
 };
 
