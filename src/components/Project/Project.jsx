213ad761e8c30db0f07bc0fadc355f1d28d62b79
Caleb Glorvigen <jcglorvi@gmail.com>
2021-03-17 21:57:07 -0400

    Added an Overlay Trigger to the buttons. Added a deployed link button.

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,23 +1,66 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Tooltip } from "react-bootstrap";
 // import Images from '../../img';
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 const Project = (props) => {
+  const renderTooltipGitHub = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Click to View the GitHub Repo
+    </Tooltip>
+  );
+
+  const renderTooltipDeployed = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Click to View the Deployed Site
+    </Tooltip>
+  );
+
   return (
-    <Card className="float-left" style={{ width: "18rem", height: "14 rem"}}>
+    <Card
+      className="float-left"
+      style={{ width: "18rem", height: "14 rem", marginRight: "5rem" }}
+    >
       {/* <Card.Img variant="top" src="https://i.ytimg.com/vi/nsw0Px-Pho8/maxresdefault.jpg" /> */}
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>{props.description}</Card.Text>
         <a className="align-bottom" href={props.gitHubRep}>
-          <Button variant="primary">GitHub</Button>
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltipGitHub}
+          >
+            <Button
+              style={{
+                textAlign: "center",
+                marginRight: "2.5rem",
+              }}
+              variant="primary"
+            >
+              GitHub
+            </Button>
+          </OverlayTrigger>
         </a>
-        {/* <a href={props.gitHubRep}>
-          <Button className="align-right" variant="primary">GitHub</Button>
-        </a> */}
+
+        <a className="align-bottom" href={props.deployLink}>
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltipDeployed}
+          >
+            <Button
+              style={{
+                textAlign: "center",
+              }}
+              variant="primary"
+            >
+              Deployed
+            </Button>
+          </OverlayTrigger>
+        </a>
       </Card.Body>
     </Card>
-    // {console.log("hello")}
   );
 };
 
