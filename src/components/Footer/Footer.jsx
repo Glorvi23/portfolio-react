ee87956ff1f4570e5e046abeb068e149b6819165
Caleb Glorvigen <jcglorvi@gmail.com>
2021-03-22 21:35:41 -0400

    Set the footer to stay at the bottom of the page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,11 +13,16 @@ import {
 
 const Footer = () => {
   return (
-    <footer style={{"margin-top": "15px","background-color": "black", "text-align": "center"}}>
-      {/* <Navbar> */}
-        <p style={{ color: "white","text-align": "center"}}>&#169; 2021 Caleb Glorvigen</p>
-      {/* </Navbar> */}
-    </footer>
+    // <footer style={{"margin-top": "15px","background-color": "black", "text-align": "center"}}>
+    //   {/* <Navbar> */}
+    //     <p style={{ color: "white","text-align": "center"}}>&#169; 2021 Caleb Glorvigen</p>
+    //   {/* </Navbar> */}
+    // </footer>
+    <footer style={{"margin-top": "8px", "color": "white", "text-align": "center"}}>
+
+      <p style={{ width: "100%", position: "absolute", bottom: 0, "margin-top": "15px", "background-color": "black", "text-align": "center"}}>&#169; 2021 Caleb Glorvigen</p>
+
+  </footer>
   );
 };
 
